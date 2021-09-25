efbd07fdbfc9e6b0199603bef3385fcd88917328
Adel Dahani <69170574+dahani9091@users.noreply.github.com>
2021-09-25 13:43:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,18 @@ import pandas as pd
 import pickle
 
 
+# Security
+#passlib,hashlib,bcrypt,scrypt
+import hashlib
+def make_hashes(password):
+    return hashlib.sha256(str.encode(password)).hexdigest()
+
+def check_hashes(password,hashed_text):
+    if make_hashes(password) == hashed_text:
+        return hashed_text
+    return False
+
+
 def recommender(brand_name,keywords=None,price_min=0,price_max=1000000,flavors=None):
 
     dataset = []
@@ -37,36 +49,116 @@ def recommender(brand_name,keywords=None,price_min=0,price_max=1000000,flavors=N
     return dataset
 
 
-st.title("Suppliment Recommandation")
+###############################################################################################################
+# DB Management
+import sqlite3 
+conn = sqlite3.connect('data.db')
+c = conn.cursor()
+# DB  Functions
+def create_usertable():
+    c.execute('CREATE TABLE IF NOT EXISTS userstable(username TEXT,password TEXT)')
 
-with st.form(key = "form1"):
-    brand_name = st.text_input(label = "Enter the product brand")
-    keywords = st.text_input(label = "Enter the keywords (They should be separated by ';' | Example : keyword1;keyword2;keyword3 )")
-    price = st.slider("Enter your budget", 1, 1000,(1, 1000))
-    flavors_options_single = st.multiselect(
-            'Enter single flavorss',
-            ['unflavored', 'strawberry', 'lemonade', 'cookie', 'pineapple', 'grape', 'raspberry', 'mint', 'pina colada', 'blueberry', 'cherry', 'candy', 'gingerbread', 'chocolate', 'peanut butter', 'maple waffle', 'fruit', 'orange', 'lemon', 'mango', 'peach', 'watermelon', 'coconut', 'vanilla', 'banana', 'apple', 'caramel', 'hazelnut', 'margarita', 'cinnamon', 'coffee', 'buttermilk', 'kiwi', 'dragon fruit', 'brownie', 'rocky road'])
+def add_userdata(username,password):
+    c.execute('INSERT INTO userstable(username,password) VALUES (?,?)',(username,password))
+    conn.commit()
 
-    flavors_options_mix = st.multiselect(
-    'Enter mix flavors',
-    ['lemonade + raspberry', 'lemonade + blueberry', 'lemonade + strawberry', 'chocolate + mint', 'chocolate + peanut butter', 'chocolate + coconut', 'chocolate + hazelnut', 'mango + peach', 'mango + lemon', 'mango + orange', 'mango + pineapple', 'banana + peanut butter', 'cherry + watermelon', 'candy + watermelon', 'cookie + peanut butter', 'coffee + caramel', 'apple + cinnamon', 'strawberry + pina colada', 'vanilla + caramel'])
-    submit = st.form_submit_button(label = "Submit")
-dataset = []
-if submit :
-    if keywords.replace('Example : keyword1;keyword2;keyword3',"").strip() != "" :
-        dataset = recommender(brand_name.strip(),keywords,int(price[0]),int(price[1]),flavors_options_single+flavors_options_mix)
-    else:
+def login_user(username,password):
+    c.execute('SELECT * FROM userstable WHERE username =? AND password = ?',(username,password))
+    data = c.fetchall()
+    return data
 
-        dataset = recommender(brand_name.strip(),price_min=int(price[0]),price_max=int(price[1]),flavors=flavors_options_single+flavors_options_mix)
-    if len(dataset) >10 :
-        df = pd.DataFrame(dataset[:10])
-        st.table(df)
-    elif len(dataset) == 0:
-        st.write("No results found")
+def view_all_users():
+    c.execute('SELECT * FROM userstable')
+    data = c.fetchall()
+    return data
 
-    else:
-        df = pd.DataFrame(dataset)
-        st.table(df)      
-    submit = False
+############################################################################################################
+
+def main():
+    """Login"""
+
+    st.title("Login")
+
+    menu = ["Home","Login","SignUp"]
+    choice = st.sidebar.selectbox("Menu",menu)
+
+    if choice == "Home":
+        st.subheader("Home")
+
+    elif choice == "Login":
+        st.subheader("Login Section")
+
+        username = st.sidebar.text_input("User Name")
+        password = st.sidebar.text_input("Password",type='password')
+        if st.sidebar.checkbox("Login"):
+            # if password == '12345':
+            create_usertable()
+            hashed_pswd = make_hashes(password)
+
+            result = login_user(username,check_hashes(password,hashed_pswd))
+            if result:
+
+                st.success("Logged In as {}".format(username))
+
+                task = st.selectbox("Task",["Personal information","Analytics"])
+                if task == "Personal information":
+                    st.subheader("Information")
+
+                elif task == "Analytics":
+                    def get_brand_names():
+                        df = pd.read_csv('tokens/combine_token_for_app.csv')
+                        df = df.drop_duplicates(subset=['brand_name'], keep='first')
+                        return list(df['brand_name'])
+                    st.subheader("Analytics")
+                    st.title("Suppliment Recommandation")
+
+                    with st.form(key = "form1"):
+                        brand_name = st.selectbox(label = "Enter the product brand",options=get_brand_names())
+                        keywords = st.text_input(label = "Enter the keywords (They should be separated by ';' | Example : keyword1;keyword2;keyword3 )")
+                        price = st.slider("Enter your budget", 1, 1000,(1, 1000))
+                        flavors_options_single = st.multiselect(
+                                'Enter single flavorss',
+                                ['unflavored', 'strawberry', 'lemonade', 'cookie', 'pineapple', 'grape', 'raspberry', 'mint', 'pina colada', 'blueberry', 'cherry', 'candy', 'gingerbread', 'chocolate', 'peanut butter', 'maple waffle', 'fruit', 'orange', 'lemon', 'mango', 'peach', 'watermelon', 'coconut', 'vanilla', 'banana', 'apple', 'caramel', 'hazelnut', 'margarita', 'cinnamon', 'coffee', 'buttermilk', 'kiwi', 'dragon fruit', 'brownie', 'rocky road'])
+
+                        flavors_options_mix = st.multiselect(
+                        'Enter mix flavors',
+                        ['lemonade + raspberry', 'lemonade + blueberry', 'lemonade + strawberry', 'chocolate + mint', 'chocolate + peanut butter', 'chocolate + coconut', 'chocolate + hazelnut', 'mango + peach', 'mango + lemon', 'mango + orange', 'mango + pineapple', 'banana + peanut butter', 'cherry + watermelon', 'candy + watermelon', 'cookie + peanut butter', 'coffee + caramel', 'apple + cinnamon', 'strawberry + pina colada', 'vanilla + caramel'])
+                        submit = st.form_submit_button(label = "Submit")
+                    dataset = []
+                    if submit :
+                        if keywords.replace('Example : keyword1;keyword2;keyword3',"").strip() != "" :
+                            dataset = recommender(brand_name.strip(),keywords,int(price[0]),int(price[1]),flavors_options_single+flavors_options_mix)
+                        else:
+
+                            dataset = recommender(brand_name.strip(),price_min=int(price[0]),price_max=int(price[1]),flavors=flavors_options_single+flavors_options_mix)
+                        if len(dataset) >10 :
+                            df = pd.DataFrame(dataset[:10])
+                            st.table(df)
+                        elif len(dataset) == 0:
+                            st.write("No results found")
+
+                        else:
+                            df = pd.DataFrame(dataset)
+                            st.table(df)      
+                        submit = False
+                
+            else:
+                st.warning("Incorrect Username/Password")
 
 
+
+
+
+    elif choice == "SignUp":
+        st.subheader("Create New Account")
+        new_user = st.text_input("Username")
+        new_password = st.text_input("Password",type='password')
+
+        if st.button("Signup"):
+            create_usertable()
+            add_userdata(new_user,make_hashes(new_password))
+            st.success("You have successfully created a valid Account")
+            st.info("Go to Login Menu to login")
+
+if __name__ == '__main__':
+	main()
